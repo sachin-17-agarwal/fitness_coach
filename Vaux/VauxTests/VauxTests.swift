70b3bf89ff582f006412fda33ff49017b6592209
sachin-17-agarwal <sachinhasitall@gmail.com>
2026-09-19 08:39:41 +1000

    Add VauxTests unit testing bundle

---
+++ Vaux/VauxTests/VauxTests.swift
@@ -0,0 +1,16 @@
+//
+//  VauxTests.swift
+//  VauxTests
+//
+//  Created by Sachin Agarwal on 19/9/2026.
+//
+
+import Testing
+
+struct VauxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
